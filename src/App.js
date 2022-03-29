b29bf6547b8b43e63d7d1a87608898ae6cdf576b
Joe Monem <jmonem@icloud.com>
2022-03-29 19:17:58 +0300

    Title & Icon

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Map from "./components/Map";
 import Launch from "./components/Launch";
+import { Helmet } from "react-helmet";
 // import Done from "./components/Done";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>AquaDAO</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <Navbar />
       <Main />
       <Map />
